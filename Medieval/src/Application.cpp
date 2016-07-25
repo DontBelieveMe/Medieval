a481fb984c16ee764168f5e6043548b7c045320f
HolyBlackCat <blckcat@inbox.ru>
2016-07-25 14:28:20 +0300

    Fixed formatting

--- Medieval/src/Application.cpp
+++ Medieval/src/Application.cpp
@@ -51,7 +51,7 @@ void Application::mainLoop()
 			glfwPollEvents();
 			delta -= 1.0;
 		}
-        
+
         float vertices[] = {
             -1.0f, -1.0f, 0.0f,
             1.0f, -1.0f, 0.0f,
--- Medieval/src/main.cpp
+++ Medieval/src/main.cpp
@@ -4,13 +4,16 @@
 
 int main()
 {
-	try {
+	try
+	{
 		Application& appInst = Application::getInstance();
 		appInst.run();
-	}catch(const std::runtime_error & e){
+	}
+	catch (const std::runtime_error & e)
+	{
 		std::cerr << e.what() << std::endl;
 		return EXIT_FAILURE;
 	}
-	
+
 	return EXIT_SUCCESS;
 }
