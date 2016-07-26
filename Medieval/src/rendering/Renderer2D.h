ae3b2dfd1720b661372d89e3d2c73116d106d286
HolyBlackCat <blckcat@inbox.ru>
2016-07-26 15:55:27 +0300

    Fixed non-const ref error

--- Medieval/src/rendering/Renderer2D.h
+++ Medieval/src/rendering/Renderer2D.h
@@ -10,9 +10,9 @@
 class Renderer2D
 {
 public:
-	Renderer2D(ShaderProgram *shader, glm::mat4& ortho);
+	Renderer2D(ShaderProgram *shader, const glm::mat4 &ortho);
 	Renderer2D(ShaderProgram *shader);
-	
+
 	void createVertexArray(float *data, int size);
 	void draw();
 	void destroy();
