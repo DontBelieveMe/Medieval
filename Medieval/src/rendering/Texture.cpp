4d332de0bc4297ee96a7ff1c82083fb9819a3040
Morgan Frary <mkfrary@gmail.com>
2016-08-03 20:25:49 +0000

    Replace Texture.cpp - Corrections to suit our coding Standard and fixed the memory leak

--- Medieval/src/rendering/Texture.cpp
+++ Medieval/src/rendering/Texture.cpp
@@ -6,7 +6,7 @@ Texture::Texture(const std::string& path)
 {
 	std::vector<GLubyte> data;
 	unsigned int error;
-	error = lodepng::decode(data, width, height, path);
+	error = lodepng::decode(data, _width, _height, path);
 	if (error) {
 		std::string errorMsg = lodepng_error_text(error);
 		std::cerr << "LodePNG Error: (" << path  << ") " << errorMsg << std::endl;
@@ -14,9 +14,20 @@ Texture::Texture(const std::string& path)
 	}
 
 	glEnable(GL_TEXTURE_2D);
-	glGenTextures(1, &texId);
-	glBindTexture(GL_TEXTURE_2D, texId);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, &data[0]);
+	glGenTextures(1, &_texId);
+	glBindTexture(GL_TEXTURE_2D, _texId);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, _width, _height, 0, GL_RGBA, GL_UNSIGNED_BYTE, &data[0]);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+Texture::Texture(const std::vector<GLubyte> &data)
+{
+	glEnable(GL_TEXTURE_2D);
+	glGenTextures(1, &_texId);
+	glBindTexture(GL_TEXTURE_2D, _texId);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, _width, _height, 0, GL_RGBA, GL_UNSIGNED_BYTE, &data[0]);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glBindTexture(GL_TEXTURE_2D, 0);
@@ -24,9 +35,5 @@ Texture::Texture(const std::string& path)
 
 Texture::~Texture()
 {
-}
-
-void Texture::destroy()
-{
-	glDeleteTextures(1, &texId);
+	glDeleteTextures(1, &_texId);
 }
