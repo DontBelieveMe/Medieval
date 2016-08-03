6c9c49d000f6b7b8007162e4a8e241f2a58d6ae2
Morgan Frary <mkfrary@gmail.com>
2016-08-03 20:22:47 +0000

    UI Renderer - the renderer for UI, uses the Primitives class for rendering. Shares a single VBO for all UI.

---
+++ Medieval/src/ui/UIRenderer.cpp
@@ -0,0 +1,68 @@
+#include "UIRenderer.h"
+#include <vector>
+
+const glm::vec2 UIRenderer::SCREENSPACE_INCREMENT = glm::vec2((2.0f / WIDTH), (2.0f / HEIGHT)), UIRenderer::SCREENSPACE_OFFSET = glm::vec2(-1.0f, 1.0f);
+#define BUFFER_OFFSET(i) ((char *)NULL + (i))
+
+UIRenderer::UIRenderer(const float &width, const float &height, const glm::vec2 &topLeftPos, ShaderProgram *shader)
+	: uiQuad(Primitives::GetUIQuad())
+{
+	_topLeftPosition = glm::vec3((topLeftPos.x) * SCREENSPACE_INCREMENT.x, -topLeftPos.y * SCREENSPACE_INCREMENT.y, 0);
+	_scale = glm::vec3(width * SCREENSPACE_INCREMENT.x, height * SCREENSPACE_INCREMENT.y, 0);
+	
+	if (shader == nullptr)
+		_uiShader = new ShaderProgram("res/shaders/UIVert.shader", "res/shaders/UIFrag.shader");
+	else
+		_uiShader = shader;
+	transScaleLocation = _uiShader->getUniformLoc("translationScale");
+}
+
+UIRenderer::UIRenderer(const glm::vec2 &topLeftPos, const glm::vec2 &bottomRightPos, ShaderProgram *shader)
+	: uiQuad(Primitives::GetUIQuad())
+{
+	float width = bottomRightPos.x - topLeftPos.x, height = topLeftPos.y - bottomRightPos.y;
+	_topLeftPosition = glm::vec3((topLeftPos.x) * SCREENSPACE_INCREMENT.x, -topLeftPos.y * SCREENSPACE_INCREMENT.y, 0);
+	_scale = glm::vec3(width * SCREENSPACE_INCREMENT.x, height * SCREENSPACE_INCREMENT.y, 0);
+
+	if (shader == nullptr)
+		_uiShader = new ShaderProgram("res/shaders/UIVert.shader", "res/shaders/UIFrag.shader");
+	else
+		_uiShader = shader;
+	transScaleLocation = _uiShader->getUniformLoc("translationScale");
+}
+
+UIRenderer::UIRenderer(const Texture &UITexture, const glm::vec2 &topLeftPos, const float &scaleValue, ShaderProgram *shader)
+	: uiQuad(Primitives::GetUIQuad())
+{
+	float width = (float)UITexture.GetWidth(), height = (float)UITexture.GetHeight();
+	_topLeftPosition = glm::vec3((topLeftPos.x) * SCREENSPACE_INCREMENT.x, -topLeftPos.y * SCREENSPACE_INCREMENT.y, 0);
+	_scale = glm::vec3(width * SCREENSPACE_INCREMENT.x * scaleValue, height * SCREENSPACE_INCREMENT.y * scaleValue, 0);
+
+	if (shader == nullptr)	
+		_uiShader = new ShaderProgram("res/shaders/UIVert.shader", "res/shaders/UIFrag.shader");	
+	else
+		_uiShader = shader;
+	transScaleLocation = _uiShader->getUniformLoc("translationScale");
+}
+
+UIRenderer::~UIRenderer()
+{
+	delete _uiShader;
+}
+
+void UIRenderer::Render()
+{
+	_uiShader->use();
+	_uiShader->uploadMatrix4f(transScaleLocation, GetMatrix());
+
+	uiQuad.Render();
+}
+
+struct Vertex
+{
+	glm::vec3 position;
+	glm::vec2 uv;
+
+	Vertex(glm::vec3 position, glm::vec2 uv)
+		: position(position), uv(uv) {}
+};
