dad1adeef76096ad2c4fabc5673d70c62b97d9cf
Morgan Frary <mkfrary@gmail.com>
2016-08-04 13:25:38 +0000

    Upload new file

---
+++ Medieval/game/uiMenus/MenuExample.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "../src/ui/UI.h"
+
+class MenuExample : public UICanvas
+{
+public:
+	MenuExample(bool enabled = true);
+	~MenuExample();
+
+	void CanvasTick();
+
+private:
+	UIButton &menuToggle, &quitButton;
+};
+
