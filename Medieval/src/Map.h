11313e159865a6b74f283fc7f71f6ba9d2f01e4e
HolyBlackCat <blckcat@inbox.ru>
2016-08-04 15:54:12 +0300

    Fixed chunk rendering.

--- Medieval/src/Map.h
+++ Medieval/src/Map.h
@@ -266,7 +266,7 @@ class Map
         glBufferData(GL_ARRAY_BUFFER, vertices * sizeof (Vertex), 0, GL_DYNAMIC_DRAW);
         for (unsigned int i = 0; i < buffer.size(); i++)
         {
-            if (i != buffer.size() - 1)
+            if (i != buffer.size() - 1 && vertices % vertices_per_buffer)
                 glBufferSubData(GL_ARRAY_BUFFER, vertices_per_buffer * sizeof (Vertex) * i, vertices_per_buffer * sizeof (Vertex), buffer[i]);
             else
                 glBufferSubData(GL_ARRAY_BUFFER, vertices_per_buffer * sizeof (Vertex) * i, (vertices % vertices_per_buffer) * sizeof (Vertex), buffer[i]);
